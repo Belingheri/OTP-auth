afd3898557bcc3fed5074bdfc78b868bf00c1347
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-04-03 14:38:18 +0200

    stampata lista di valori salvati

--- js/configOTP.js
+++ js/configOTP.js
@@ -6,10 +6,11 @@ function salvaDatiQr() {
   obj.secret = document.getElementById("secret").value;
   obj.issuer = document.getElementById("issuer").value;
   obj.type = document.getElementById("type").value;
+  obj.counter = document.getElementById("counter").value || 0;
 
   try {
     OPTService.save(obj);
-    stampaListaOTPSalvati("listaSalvati");
+    stampaListaOTPSalvati();
   } catch (error) {
     alert(error.message);
   }
@@ -19,15 +20,37 @@ function salvaDatiQr() {
  *
  * @param {string} nomeOlElement nome elemento ol dove disegnare la lista
  */
-function stampaListaOTPSalvati(nomeOlElement) {
+function stampaListaOTPSalvati(nomeOlElement = "listaSalvati") {
   const ol = document.getElementById(nomeOlElement);
   ol.innerHTML = "";
   OPTService.getAll().forEach((e) => {
-    const li = document.createElement("li", e);
+    const li = document.createElement("li");
     li.classList.add("list-group-item");
-    li.innerHTML = e.id;
+    li.id = e.id;
     ol.appendChild(li);
+
+    const spanTitle = document.createElement("u");
+    spanTitle.setAttribute("role", "button");
+    spanTitle.innerHTML = e.id;
+    li.appendChild(spanTitle);
+
+    const spanType = document.createElement("span");
+    spanType.innerHTML = e.type.toUpperCase();
+    spanType.classList.add("badge", "badge-info", "m-1");
+    li.appendChild(spanType);
+
+    const spanDel = document.createElement("span");
+    spanDel.innerHTML = "X";
+    spanDel.classList.add("badge", "badge-danger", "float-right", "my-1");
+    spanDel.setAttribute("role", "button");
+    spanDel.onclick = rimuoviOTP;
+    li.appendChild(spanDel);
   });
+}
+
+function rimuoviOTP({ currentTarget }) {
+  OPTService.deleteOne(currentTarget.parentNode.id);
+  stampaListaOTPSalvati();
 }
 
 export { salvaDatiQr, stampaListaOTPSalvati };
