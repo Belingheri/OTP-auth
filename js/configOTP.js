c5d259850de5a943c5e9d6bc20909f78e2e65c8a
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-04-03 15:34:09 +0200

    only lecters support

--- js/configOTP.js
+++ js/configOTP.js
@@ -69,4 +69,14 @@ function rimuoviOTP({ currentTarget }) {
   stampaListaOTPSalvati();
 }
 
-export { salvaDatiQr, stampaListaOTPSalvati };
+function randomString(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+export { salvaDatiQr, stampaListaOTPSalvati, randomString };
