e518f7f99e98bae72509cef2aa339d46256994d3
Riccardo Belingheri <riccardo.beli99@gmail.com>
2021-04-03 14:06:17 +0200

    stampa tutti OTP salvati

--- js/configOTP.js
+++ js/configOTP.js
@@ -9,9 +9,25 @@ function salvaDatiQr() {
 
   try {
     OPTService.save(obj);
+    stampaListaOTPSalvati("listaSalvati");
   } catch (error) {
     alert(error.message);
   }
 }
 
-export { salvaDatiQr };
+/**
+ *
+ * @param {string} nomeOlElement nome elemento ol dove disegnare la lista
+ */
+function stampaListaOTPSalvati(nomeOlElement) {
+  const ol = document.getElementById(nomeOlElement);
+  ol.innerHTML = "";
+  OPTService.getAll().forEach((e) => {
+    const li = document.createElement("li", e);
+    li.classList.add("list-group-item");
+    li.innerHTML = e.id;
+    ol.appendChild(li);
+  });
+}
+
+export { salvaDatiQr, stampaListaOTPSalvati };
